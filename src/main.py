ab1ad9865bb8634685f55c8406fd4ba26776c01d
Lee Fergusson <lee.fergusson@googlemail.com>
2025-01-07 17:03:53 +0000

    feat: add TextNode class and TextType enum

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is a text node", TextType.NORMAL_TEXT, "https://www.boot.dev")
+    print(node)
+
+main()
